177d415355e0dca188aeac76c788203d0b23bb77
Joey3011 <jeff.me.7919@gmail.com>
2022-11-20 15:33:23 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+const nameInput = document.getElementById("nameInput")
+const lbs = document.getElementById("lbs")
+const output = document.getElementById("output")
+const submit = document.getElementById("submit")
+const clear = document.getElementById("clear")
+
+class Abrakadabra{
+    constructor (varName, kg){
+        this.varName = varName
+        this.kg = Number(kg)
+        this.lbs = kg / 0.45359237
+    }
+    
+    checkWeight = (targetInfo) => {
+        let result = []
+        if(targetInfo.varName != "" && !isNaN(targetInfo.kg) && targetInfo.kg != ""){
+            //view output in web page
+            result.push(`${targetInfo.varName}'s weight in lbs is ${targetInfo.lbs.toFixed(2)}`)
+            output.innerHTML = result.join("")
+            //view output in console
+            console.log(`${targetInfo.varName}'s weight in lbs is ${targetInfo.lbs.toFixed(2)}`)  
+            return targetInfo
+        }else{
+            alert("Inputs are required and lbs should be number... THANK YOU!")
+            lbs.value = ""
+            return new Error("Inputs are required and lbs should be number... THANK YOU!")
+        }
+    }
+}
+
+
+clear.addEventListener('click', () =>{
+    output.innerHTML = ""
+    nameInput.value = ""
+    lbs.value = ""
+})
+
+submit.addEventListener('click', () => {
+    let ayiee = new Abrakadabra(nameInput.value, lbs.value); 
+    console.log(ayiee.checkWeight(ayiee))  
+})
+
+
+
